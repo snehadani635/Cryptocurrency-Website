419647d07321f499112ed4c1dbf7e3ac07a689bc
Learning <snehadani635@gmail.com>
2026-07-15 16:28:33 +0530

    add buttons with right position

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+var settings = {
+    "async": true,
+    "scrossDomain" : true,
+    "url" : "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+    "method": "GET"
+}
